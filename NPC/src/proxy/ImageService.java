8fb552a8d8f49c619dee16011e0b8ec7c7402b32
Cong Phuong <congphuong.vietnam62@gmail.com>
2022-12-20 17:10:06 +0700

    proxy

---
+++ NPC/src/proxy/ImageService.java
@@ -0,0 +1,6 @@
+package proxy;
+
+public interface ImageService {
+
+    void showImage();
+}
---
+++ NPC/src/proxy/Main.java
@@ -0,0 +1,18 @@
+package proxy;
+
+public class Main {
+
+    //Cãi thiện Performance thông qua lazy loading, chỉ tải các tài nguyên khi chúng được yêu cầu.
+    //Nó cung cấp sự bảo vệ cho đối tượng thực từ thế giới bên ngoài.
+    //Giảm chi phí khi có nhiều truy cập vào đối tượng có chi phí khởi tạo ban đầu lớn.
+    //Dễ nâng cấp, bảo trì.
+
+    public static void main(String[] args) {
+        System.out.println("Init proxy");
+        ProxyImage proxyImage = new ProxyImage("https://google.com");
+        System.out.println("Call proxy 1st");
+        proxyImage.showImage();
+        System.out.println("Call proxy 2nd");
+        proxyImage.showImage();
+    }
+}
